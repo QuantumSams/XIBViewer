c6d2d9a1715bed8ba4ec379af563967f1f813376
QuantumCat <tranhoangphuchuy04@gmail.com>
2024-06-14 22:54:26 +0700

    chore: refactor SigninVC

--- XIBViewer/View/SignIn/SignInVC.swift
+++ XIBViewer/View/SignIn/SignInVC.swift
@@ -1,37 +1,47 @@
-//
-//  SignInVC.swift
-//  XIBViewer
-//
-//  Created by Huy on 28/5/24.
-//
-
 import UIKit
 
-class SignInVC: UIViewController {
+final class SignInVC: UIViewController {
 
-    @IBOutlet var usernameField: UITextField!
-    @IBOutlet var passwordField: UITextField!
-    @IBOutlet var loginButton: UIButton!
+    //Outlet
+    @IBOutlet private weak var passwordField: UITextField!
+    @IBOutlet private weak var usernameField: UITextField!
+    @IBOutlet private weak var loginButton: UIButton!
     
+    //Life cycle
     override func viewDidLoad() {
-        usernameField.layer.cornerRadius = 20
-        usernameField.layer.borderColor = UIColor.systemBlue.cgColor
-        usernameField.layer.borderWidth = 1
-        usernameField.layer.masksToBounds = true
-        usernameField.borderStyle = .roundedRect
-        
-        passwordField.layer.cornerRadius = 20
-        passwordField.layer.borderColor = UIColor.systemBlue.cgColor
-        passwordField.layer.borderWidth = 1
-        passwordField.layer.masksToBounds = true
-        passwordField.borderStyle = .roundedRect
-        
-        loginButton.layer.cornerRadius = 16
-        loginButton.layer.masksToBounds = true
-        
         super.viewDidLoad()
+        setupViews()
     }
+    
+    //Action - event processing
    
     @IBAction func loginButtonTapped(_ sender: UIButton) {
+        print("Login button clicked")
     }
 }
+
+//Private method
+
+extension SignInVC{
+    private func setupViews(){
+        setupTextField(usernameField)
+        setupTextField(passwordField)
+        setupButton(loginButton)
+    }
+    
+    private func setupTextField(_ customTextField:UITextField){
+        customTextField.layer.cornerRadius = 20
+        customTextField.layer.borderColor = UIColor.systemBlue.cgColor
+        customTextField.layer.borderWidth = 1
+        customTextField.layer.masksToBounds = true
+        customTextField.borderStyle = .roundedRect
+        
+        NSLayoutConstraint.activate([customTextField.heightAnchor.constraint(equalToConstant: 64)])
+    }
+    
+    private func setupButton(_ customButton: UIButton){
+        customButton.layer.cornerRadius = 16
+        customButton.layer.masksToBounds = true
+    }
+}
+
