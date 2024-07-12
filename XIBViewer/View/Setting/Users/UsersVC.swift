21eaffffb6c4538f91f6a4dffebd9c0f6b36aef4
QuantumCat <tranhoangphuchuy04@gmail.com>
2024-07-12 13:35:07 +0700

    Add: reload table

--- XIBViewer/View/Setting/Users/UsersVC.swift
+++ XIBViewer/View/Setting/Users/UsersVC.swift
@@ -8,11 +8,12 @@ class UsersVC: UIViewController {
         }
     }
 
+    private var nextURL: String?
+
     @IBOutlet private weak var userTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        requestGetListAPI()
+        requestNewUsersList()
         setupViews()
     }
 }
@@ -31,38 +32,96 @@ extension UsersVC: UITableViewDataSource, UITableViewDelegate{
         return returnCell
     }
     
+    func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+
+        // UITableView only moves in one direction, y axis
+        let currentOffset = scrollView.contentOffset.y
+        let maximumOffset = scrollView.contentSize.height - scrollView.frame.size.height
+
+        // Change 10.0 to adjust the distance from bottom
+        if maximumOffset - currentOffset <= 10.0 {
+            requestUpdateUserList(url: nextURL)
+        }
+    }
+    
     private func reloadTable(for table: UITableView){
         DispatchQueue.main.async {
             table.reloadData()
         }
     }
     
+    
+    
     private func setupViews(){
         userTableView.dataSource = self
         userTableView.delegate = self
         userTableView.register(UsersTableCell.getNib(), forCellReuseIdentifier: UsersTableCell.getID())
+        pullToRefresh()
+    }
+    
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        DispatchQueue.main.async {[weak self] in
+            guard let self = self else {return}
+            
+            
+            let currentOffset = scrollView.contentOffset.y
+            let maximumOffset = scrollView.contentSize.height - scrollView.frame.size.height
+          
+            if maximumOffset - currentOffset <= 10.0 {
+                self.requestUpdateUserList(url: nextURL)
+            }
+        }
+    }
+}
+
+extension UsersVC{
+    
+    @objc func pullToRequestAction(){
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            guard let self = self else {return}
+            
+            self.requestNewUsersList(loadingAnimation: false)
+            self.userTableView.refreshControl?.endRefreshing()
+        }
+    }
+    
+    
+    private func pullToRefresh(){
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self,
+                                 action: #selector(pullToRequestAction),
+                                 for: .valueChanged)
+        
+        userTableView.refreshControl = refreshControl
     }
 }
 
 
 extension UsersVC{
-    private func requestGetListAPI(limit: Int = 10, offset: Int = 0){
+    private func requestNewUsersList(limit: Int = 10, offset: Int = 0, loadingAnimation: Bool = true){
         guard let request = Endpoints.getUserList(limit: limit, offset: offset).request else{
             return
         }
+        requestNewUsersListAPI(with: request, loadingAnimation: loadingAnimation)
+    }
+    
+    
+    private func requestNewUsersListAPI(with request: URLRequest, loadingAnimation: Bool = true){
+
         DispatchQueue.main.async { [weak self] in
             guard let self = self else {return}
-            self.startIndicatingActivity()
+            if loadingAnimation {self.startIndicatingActivity()}
             AccountService.getUsersList(request: request) { result in
                 switch result{
                 case .success(let data):
-                    self.stopIndicatingActivity()
                     self.userList = data.results
-                    self.reloadTable(for: self.userTableView)
+                    self.nextURL = data.next
                     
                 case .failure(let error):
                     guard let error = error as? APIErrorTypes else {return}
-                    self.stopIndicatingActivity()
+                    
                     switch error{
                     case .serverError(let string):
                         AlertManager.showServerErrorResponse(on: self, message: string)
@@ -73,6 +132,45 @@ extension UsersVC{
                         AlertManager.showDeviceError(on: self, message: string)
                     }
                 }
+                if loadingAnimation {self.stopIndicatingActivity()}
+            }
+        }
+    }
+    
+    private func requestUpdateUserList(url: String?){
+        
+        guard let url = url,
+              let path = URL(string: url),
+              let request = Endpoints.loadMoreUserList(fullPath: path).request else{
+            return
+        }
+        requestUpdateUserListAPI(with: request)
+    }
+    
+    private func requestUpdateUserListAPI(with request: URLRequest){
+
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else {return}
+            self.startIndicatingActivity()
+            AccountService.getUsersList(request: request) { result in
+                switch result{
+                case .success(let data):
+                    self.userList = self.userList + data.results
+                    self.nextURL = data.next
+                    
+                case .failure(let error):
+                    guard let error = error as? APIErrorTypes else {return}
+                    switch error{
+                    case .serverError(let string):
+                        AlertManager.showServerErrorResponse(on: self, message: string)
+                    case .decodingError(let string),
+                            .unknownError(let string):
+                        AlertManager.showDevelopmentError(on: self, message: string, errorType: .decodingError())
+                    case .deviceError(let string):
+                        AlertManager.showDeviceError(on: self, message: string)
+                    }
+                }
+                self.stopIndicatingActivity()
             }
         }
     }
@@ -81,11 +179,8 @@ extension UsersVC{
 extension UsersVC{
     private func confirmDeletion(id: Int){
         AlertManager.deleteUserConfirm(on: self) { [weak self] choice in
-            switch choice{
-            case true:
+            if choice == true{
                 self?.requestDeleteUserAPI(id: id)
-            case false:
-                break
             }
         }
     }
@@ -96,23 +191,19 @@ extension UsersVC{
         }
         
         AccountService.deleteUser(request: request) { [weak self] result in
-            DispatchQueue.main.async {
+            DispatchQueue.main.async {[weak self] in
                 guard let self = self else {return}
-                
                 self.startIndicatingActivity()
-                
                 switch result{
-                case .success():
+                case .success(_):
                     AlertManager.showAlert(on: self,
                                            title: "Request completed",
                                            message: "User has been removed.")
                     
-                    self.stopIndicatingActivity()
-                    self.requestGetListAPI()
+                    self.requestNewUsersList()
                     
                 case .failure(let error):
                     guard let error = error as? APIErrorTypes else {return}
-                    self.stopIndicatingActivity()
                     switch error{
                     case .serverError(let string):
                         AlertManager.showServerErrorResponse(on: self, message: string)
@@ -123,6 +214,7 @@ extension UsersVC{
                         AlertManager.showDeviceError(on: self, message: string)
                     }
                 }
+                self.stopIndicatingActivity()
             }
         }
     }
@@ -151,6 +243,6 @@ extension UsersVC: EditVCDelegate{
         self.presentCustomVCWithNavigationController(toVC: vc)
     }
     func doneEditing(send newUserData: UserModel) {
-        requestGetListAPI()
+        requestNewUsersList()
     }
 }
