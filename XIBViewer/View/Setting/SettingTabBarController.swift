d5fc76abc9ff83bdbaddf52dc63fd511603e9d8e
QuantumCat <tranhoangphuchuy04@gmail.com>
2024-07-13 11:13:19 +0700

    Chore: MVVM on SettingNavView

--- XIBViewer/View/Setting/SettingTabBarController.swift
+++ XIBViewer/View/Setting/SettingTabBarController.swift
@@ -1,23 +1,20 @@
-
 import UIKit
 
 final class SettingTabBarController: UITabBarController {
-    
-    
+    // MARK: - VARIABLES
+
+    // MARK: - OUTLETS
+
+    // MARK: - LIFECYCLE
+
     init() {
         super.init(nibName: nil, bundle: nil)
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    let tabBarItemFontAttribute: [NSAttributedString.Key : Any] = [
-        NSAttributedString.Key.font: UIFont.systemFont(
-            ofSize: Constant.TabBarConstant.fontSize,
-            weight: UIFont.Weight.bold
-        ),
-    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,51 +26,47 @@ final class SettingTabBarController: UITabBarController {
         setupTabBarPosition()
     }
 }
-
-extension SettingTabBarController: UITabBarControllerDelegate{
-    
-    private func setupNav(){
-        initialSetup()
-        
-        let firstTab = modifyVC(viewController: AccountVC(), title: "Account", tag: 0)
-        let secondTab = modifyVC(viewController: UsersVC(), title: "Users", tag: 1)
-        
-        setViewControllers([firstTab, secondTab], animated: true)
-        tabBar.tintColor = .systemPurple
-        selectedIndex = 0
-    }
-    
-    private func modifyVC(viewController: UIViewController, title: String, tag:Int) -> UIViewController{
+// MARK: - DELEGATE/DATASOURCE CONFORM
+//UITabBarControllerDelegate
+extension SettingTabBarController: UITabBarControllerDelegate {
+    private func modifyVC(viewController: UIViewController, title: String, tag: Int) -> UIViewController {
         let returningVC = viewController
         returningVC.tabBarItem = UITabBarItem(title: title, image: nil, tag: tag)
         returningVC.tabBarItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -10)
         
         return returningVC
     }
+}
+//MARK: - ADDITIONAL METHODS
+//Setup Views
+extension SettingTabBarController{
+    private func setupNav() {
+        initialSetup()
+        
+        let firstTab = modifyVC(viewController: AccountVC(), title: "Account", tag: 0)
+        let secondTab = modifyVC(viewController: UsersVC(), title: "Users", tag: 1)
+        
+        setViewControllers([firstTab, secondTab], animated: true)
+        selectedIndex = 0
+    }
     
-    private func initialSetup(){
-        self.delegate = self
+    
+    private func initialSetup() {
+        let tabBarItemFontAttribute: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.font: UIFont.systemFont(
+                ofSize: Constant.TabBarConstant.fontSize,
+                weight: UIFont.Weight.bold
+            ),
+        ]
+        delegate = self
         title = "Setting"
         navigationController?.navigationBar.prefersLargeTitles = true
         UITabBarItem.appearance().setTitleTextAttributes(tabBarItemFontAttribute, for: .normal)
     }
-    
-    private func setupTabBarPosition(){
+    private func setupTabBarPosition() {
         let height = navigationController?.navigationBar.frame.maxY
         tabBar.frame = CGRect(x: 0, y: height ?? 0, width: tabBar.frame.size.width, height: Constant.TabBarConstant.height)
         tabBar.isOpaque = false
         tabBar.barTintColor = Constant.TabBarConstant.backgroundColor
-    }
-    
-    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-        switch(tabBar.selectedItem?.tag){
-        case 0: tabBar.tintColor = UIColor.systemPurple
-            break
-        case 1: tabBar.tintColor = UIColor.systemBlue
-            break
-        default:
-            break
-            
-        }
     }
 }
