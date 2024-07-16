0c4e34c39a0c968e2412441dfcc3b6601f5f04f1
QuantumCat <tranhoangphuchuy04@gmail.com>
2024-07-16 21:37:03 +0700

    Chore: migrate RoleListRepositoryRemotedDataSourceImp to use generic network layer

--- XIBViewer/Data/DataSources/Remote/RoleRepositoryRemotedDataSource.swift
+++ XIBViewer/Data/DataSources/Remote/RoleRepositoryRemotedDataSource.swift
@@ -6,8 +6,13 @@ protocol RoleListRepositoryRemoteDataSource{
 
 
 final class RoleListRepositoryRemotedDataSourceImp: RoleListRepositoryRemoteDataSource {
+    
+    let networkService = NetworkService()
+    
     func getRoleList(completion: @escaping ((Result<[RoleModel], any Error>) -> Void)) {
-        RoleService.getRole { result in
+        guard let request = Endpoints.getRole().request else {return}
+        
+        networkService.sendRequest(urlRequest: request) { (result: Result<RoleListResponseDTO, APIErrorTypes>) in
             switch result {
             case .success(let data):
                 completion(.success(data.toDomain()))
